94446d2aa6479842e843d2bba280e8d6a1fe569f
Futo-Fukuyama <65376683+Futo-Fukuyama@users.noreply.github.com>
2020-05-29 09:41:35 +0900

    change from JDK1_8 to JDK1_7

--- app/src/main/java/com/example/bttracker/MainActivity.java
+++ app/src/main/java/com/example/bttracker/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.bttracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToLogacvtivity(View view) {
+        Intent toLog = new Intent(this, Logacvtivity.class);
+        startActivity(toLog);
+    }
+
+    public void goToNormalActivity(View view) {
+        Intent toNormal = new Intent(this, NormalActivity.class);
+        startActivity(toNormal);
+
+
     }
 }
